93567d10b63e0efdf6c07e979df2b52b91a2983c
Cosmin Roger Harsulescu <Cosminroger@gmail.com>
2021-04-02 17:16:25 +0200

    Attempt to fix

--- Memoar/Memoar/memoarModel.swift
+++ Memoar/Memoar/memoarModel.swift
@@ -26,7 +26,6 @@ class memoarModel {
         chunk.setSlot(slot: "background", value: background)
         model.buffers["visual"] = chunk
         
-        print(((NSDate().timeIntervalSince1970 * 1000 - time)/1000))
         model.time += ((NSDate().timeIntervalSince1970 * 1000 - time)/1000)
         time = NSDate().timeIntervalSince1970 * 1000
         
@@ -46,6 +45,7 @@ class memoarModel {
         time = NSDate().timeIntervalSince1970 * 1000
         
         model.run()
+
         return model.lastAction(slot: "isa")
 
     }
